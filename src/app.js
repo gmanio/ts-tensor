b5b0975808fbaee2fe44604477d87b0c9f4aecee
Jiman Park <im@gman.io>
2019-01-22 23:18:56 +0900

    [feature] added liteserver

--- src/app.js
+++ src/app.js
@@ -3,7 +3,32 @@
 // import * as tf from '@tensorflow/tfjs';
 // import { loadFrozenModel } from '@tensorflow/tfjs-converter';
 
+const MODEL_URL = 'tensor/web_model/tensorflowjs_model.pb';
+const WEIGHTS_URL = 'tensor/web_model/weights_manifest.json';
+
+const getModel = async () => {
+  const model = await tf.loadFrozenModel(MODEL_URL, WEIGHTS_URL);
+
+
+
+  return model;
+};
+
+
 class Greetings extends React.Component {
+  constructor() {
+    super();
+    const model = getModel();
+    
+    model.then((test)=>{
+      const daisy = document.getElementById('daisy');
+      test.execute({ input: tf.fromPixels(daisy) });
+      // tf.excute({ input: tf.fromPixels(daisy) });
+    })
+    // debugger;
+    // model.excute({ input: tf.fromPixels(daisy) })
+  }
+
   render() {
     return React.createElement('h1', null, 'Greetings, ' + this.props.name + '!');
   }
